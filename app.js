d849d5cec0b8c1caccdecb2f0bca13006b3fdbd9
Artem <guts@Artems-MacBook-Air.local>
2019-12-08 12:20:03 -0800

    add more holding features

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ scores = [0, 0];
 p1 = 0;
 p2 = 1;
 
+var curPlayer = 0;
+//p1 is first
+
 //resets
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
@@ -24,8 +27,23 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
 	var d = Math.floor(Math.random() * 6) + 1;
 	diceDom = document.querySelector(".dice");
 	diceDomImg = "dice-" + d + ".png";
-	document.querySelector(".dice").src = diceDomImg;
+	//document.querySelector(".dice").src = diceDomImg;
+	diceDom.src = diceDomImg;
 	document.querySelector(".dice").style.display = "block";
 
+	if (d != 1) {
+		p1 += d;
+		document.getElementById("current-" + curPlayer).textContent = p1;
+	} else {
+		document.getElementById("current-" + curPlayer).textContent = 0;
+	}
+
 	console.log(d);
+});
+
+document.querySelector(".btn-hold").addEventListener("click", () => {
+	//player turn flipped after hold is clicked
+	scores[curPlayer] = p1;
+	document.getElementById(".score-0") = p1;
+	curPlayer = curPlayer === 0 ? 1 : 0;
 });
